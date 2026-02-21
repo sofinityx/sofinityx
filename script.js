bfc703f9fb0d7a621e372a036954169f7508a7f5
SofinityX <SofinityX@gmail.com>
2026-02-21 22:40:07 +0300

    Remove cursor glow effect

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const art = document.querySelector('.brand-art');
-const glow = document.getElementById("cursor-glow");
 
 let currentX = 0;
 let currentY = 0;
@@ -10,17 +9,6 @@ document.addEventListener('mousemove', (e) => {
   const { innerWidth, innerHeight } = window;
   targetX = (e.clientX / innerWidth - 0.5) * 18;
   targetY = (e.clientY / innerHeight - 0.5) * 18;
-
-  const x = (e.clientX / window.innerWidth) * 100;
-  const y = (e.clientY / window.innerHeight) * 100;
-
-  glow.style.setProperty("--x", `${x}%`);
-  glow.style.setProperty("--y", `${y}%`);
-  glow.style.opacity = "0.35";
-});
-
-document.addEventListener("mouseleave", () => {
-  glow.style.opacity = "0";
 });
 
 function animate() {
